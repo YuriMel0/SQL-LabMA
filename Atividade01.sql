1794db59560ab20649b4816ecce4d625280f3839
YuriMel0 <yurimelo@ufrj.br>
2023-02-01 15:42:00 -0300

    adicionando ex02

--- Atividade01.sql
+++ Atividade01.sql
@@ -2,3 +2,8 @@
 SELECT * FROM Customers
 WHERE Country = 'Brazil';
 
+/* 2 - Liste os clientes (CustomerName), e seus endereços (Address), das cidades de “São Paulo” ou do “Rio de Janeiro”.*/
+SELECT CustomerName, Address
+FROM Customers
+WHERE City = 'Rio de Janeiro' or 'São Paulo';
+
