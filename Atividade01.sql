1514eb901de7b7336f1d3563ff794a66c08a0cb0
YuriMel0 <yurimelo@ufrj.br>
2023-02-03 19:39:37 -0300

    adicionando ex03

--- Atividade01.sql
+++ Atividade01.sql
@@ -7,3 +7,8 @@ SELECT CustomerName, Address
 FROM Customers
 WHERE City = 'Rio de Janeiro' or 'São Paulo';
 
+
+/* 3 - Liste o nome do contato (ContactName) iniciado pela letra ‘A’ e seu países. */
+SELECT ContactName, Country
+FROM Customers
+WHERE ContactName LIKE 'A%';
