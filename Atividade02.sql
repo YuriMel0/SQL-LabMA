b99e98fed613b63d4b6fce5a8c1602d90292dbce
YuriMel0 <yurimelo@ufrj.br>
2023-02-06 19:33:15 -0300

    adicionando ex02

--- Atividade02.sql
+++ Atividade02.sql
@@ -3,3 +3,8 @@ SELECT *
 FROM at_mor
 WHERE DATA_NASC = '201103';
 
+/* 2 - Quais são os CPF e Sexo dos segurados ativos nascidos em junho de 2010? */
+SELECT CPF, SEXO 
+FROM at_mor
+WHERE DATA_NASC = '201006';
+
