6cff13b5baa27de0827b1d7e41572b1f770cc43d
YuriMel0 <yurimelo@ufrj.br>
2023-02-06 19:52:23 -0300

    adicionando ex03

--- Atividade02.sql
+++ Atividade02.sql
@@ -8,3 +8,8 @@ SELECT CPF, SEXO
 FROM at_mor
 WHERE DATA_NASC = '201006';
 
+/* 3 - Quais são os Produto comprados por segurados ativos nascidos em
+       junho de 2010 e maio de 2011 e em que empresa? */
+SELECT PRODUTO
+FROM at_mor
+WHERE DATA_NASC = '201006' AND '201103';
